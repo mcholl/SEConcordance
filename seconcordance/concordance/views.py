f444430639eac3e629b0167f35fc85992f3fe030
Michael Hollinger <mcholl@yahoo.com>
2014-03-03 22:46:22 -0500

    Minor fixes

--- seconcordance/concordance/views.py
+++ seconcordance/concordance/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from VerseReference import *
 import pprint
@@ -8,9 +9,24 @@ import pprint
 from concordance.models import *
 
 # Create your views here.
+per_page_count=100
+
 def index(request):
 
-	found_references_list = VerseReference.objects.order_by('book_num', 'start_chapter', 'start_verse')
+	found_references = VerseReference.objects.order_by('book_num', 'start_chapter', 'start_verse')
+	paginator = Paginator(found_references, per_page_count)
+
+	page = request.GET.get('page')
+	try:
+ 		found_references_list = paginator.page(page)
+ 	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+ 		found_references_list = paginator.page(1)
+ 	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		found_references_list = paginator.page(paginator.num_pages)
+
+	#return render_to_response('concordance/index.html', {"found_references_list": found_references_list})
 
 	template = loader.get_template('concordance/index.html')
 	context = RequestContext( request, {
@@ -25,16 +41,27 @@ def search(request, filter_range):
 	search_ref = BibleReference(filter_range)
 	qry_in_range = "SELECT * FROM concordance_reference WHERE ref_book_num>=%s AND end_book_num<=%s AND ref_endchapter_num >= %s AND ref_endverse_num >= %s AND ref_startchapter_num <= %s AND ref_startverse_num <= %s ORDER BY ref_book_num, ref_startchapter_num, ref_startverse_num, ref_endchapter_num, ref_endverse_num"
 	params = tuple([search_ref.book_num, search_ref.end_book_num, search_ref.start_chapter, search_ref.start_verse, search_ref.end_chapter, search_ref.end_verse]) 
-	# qry_in_range = "SELECT * FROM concordance_reference WHERE ref_book_num=%s AND ref_endchapter_num >= %s AND ref_endverse_num >= %s AND ref_startchapter_num <= %s AND ref_startverse_num <= %s ORDER BY ref_book_num, ref_startchapter_num, ref_startverse_num, ref_endchapter_num, ref_endverse_num"
-	# params = tuple([search_ref.book_num, search_ref.start_chapter, search_ref.start_verse, search_ref.end_chapter, search_ref.end_verse]) 
 
-	found_references_list = VerseReference.objects.raw(qry_in_range, params)
+	found_references = list(VerseReference.objects.raw(qry_in_range, params))
+	paginator = Paginator(found_references, per_page_count)
+
+	page = request.GET.get('page')
+	try:
+ 		found_references_list = paginator.page(page)
+ 	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+ 		found_references_list = paginator.page(1)
+ 	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		found_references_list = paginator.page(paginator.num_pages)
+
+
+	#return render_to_response('concordance/index.html', {"found_references_list": found_references_list})
 
 	template = loader.get_template('concordance/index.html')
 	context = RequestContext( request, {
-		'filter_range': filter_range,
-		'filter_ref': search_ref,
 		'found_references_list': found_references_list,
+		'filter_range': filter_range,
 		})
 	return HttpResponse(template.render(context))
 
